3dad977aff90eea451dccbfee30179964d1c59a7
Puppy <100547521+hoangk3@users.noreply.github.com>
2026-08-01 09:38:42 +0700

    Create dem tp lien thong.cpp

---
+++ DFS/dem tp lien thong.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int,int>;
+using pll = pair<long long, long long>;
+
+#define pb push_back;
+#define fi first;
+#define se second;
+#define all(x) (x), begins(), (x).end()
+
+inline void fastio()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+}
+//ppz4nx solution
+vector<ll> adj[10005];
+bool visited[10005];
+ll gg = 0;
+void dfs(ll u)
+{
+    visited[u] = true;
+    for ( auto v: adj[u])
+    {
+        if(visited[v]== false)
+        {
+            dfs(v);
+        }
+    }
+
+}
+int main()
+{
+    bool check = false;
+    ll n,m;
+    cin >> n >> m;
+    for ( ll i = 1; i <= m; i++)
+    {
+        ll u,v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+    for ( int j = 1; j <= n;j++)
+    {
+        if( !visited[j])
+        {
+            dfs(j);
+            gg++;
+        }
+    }
+    cout << gg;
+}
