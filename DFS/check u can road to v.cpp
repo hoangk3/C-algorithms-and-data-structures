89141ae4050b22eb43400e1b60df9aa106884350
Puppy <100547521+hoangk3@users.noreply.github.com>
2026-08-02 08:06:54 +0700

    Create check u can road to v.cpp

---
+++ DFS/check u can road to v.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int,int>;
+using pll = pair<long long, long long>;
+
+#define pb push_back;
+#define fi first;
+#define se second;
+#define all(x) (x), begins(), (x).end()
+
+inline void fastio()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+}
+//ppz4nx solution
+vector<ll> adj[10006];
+bool visited[10005];
+ll t,s;
+void dfs( ll u)
+{
+    visited[u] = true;
+    for ( auto v : adj[u])
+    {
+        if ( visited[v] == false)
+        {
+            dfs(v);
+        }
+    }
+}
+int main()
+{
+    ll n,m;
+    cin >> n >> m;
+    for ( ll i = 1; i <= m; i++)
+    {
+        ll u,v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    cin >> s >> t;
+    dfs(s);
+    if ( visited[t])
+    {
+        cout << "yes";
+
+    }
+    else
+    {
+        cout << "no";
+    }
+}
+
